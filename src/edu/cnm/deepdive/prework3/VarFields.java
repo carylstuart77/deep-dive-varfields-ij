f74e884c46bad64168168c56b14d541970754fee
caryl <carylstuart77@gmail.com>
2017-09-20 18:19:48 -0600

    Prework 3 fields displaying room content.

---
+++ src/edu/cnm/deepdive/prework3/VarFields.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.prework3;
+
+public class VarFields {
+  static private int chairqty =4;
+  static private int tableqty = 1;
+  static private int sidetableqty = 1;
+  static private int trashqty = 1;
+  /**
+   * Prework 3 Fields
+   */
+  public static void main(String[] args) {
+    System.out.println ("Room Content");
+    System.out.println("Chairs: " + chairqty);
+    System.out.println("Tables: " + tableqty);
+    System.out.println("Side Table: " + sidetableqty);
+    System.out.println("Trash Cans: " + trashqty);
+
+  }
+}
