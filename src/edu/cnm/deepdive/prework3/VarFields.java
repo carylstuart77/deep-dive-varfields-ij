6b8114b17fe39af7b24ad8f85bb2bcd1ae2730cc
caryl <carylstuart77@gmail.com>
2017-09-21 11:02:09 -0600

    Prework 3 fields displaying room content wit excercise local and static components.

--- src/edu/cnm/deepdive/prework3/VarFields.java
+++ src/edu/cnm/deepdive/prework3/VarFields.java
@@ -1,19 +1,33 @@
 package edu.cnm.deepdive.prework3;
 
 public class VarFields {
+ // Class Variables in Static fields
+  /** static String cv = "by Class Static Fields";
   static private int chairqty =4;
   static private int tableqty = 1;
   static private int sidetableqty = 1;
   static private int trashqty = 1;
-  /**
-   * Prework 3 Fields
-   */
+  static String gd = "Good Day";
+  */
+
+  //Prework 3 Fields
+  
   public static void main(String[] args) {
-    System.out.println ("Room Content");
+    //Local Variables to method main
+    String cv = "by method Local Variables ";
+    int chairqty =4;
+    int tableqty = 1;
+    int sidetableqty = 1;
+    int trashqty = 1;
+    String gd = "Good Day";
+
+
+    System.out.println ("Room Content " + cv);
     System.out.println("Chairs: " + chairqty);
     System.out.println("Tables: " + tableqty);
     System.out.println("Side Table: " + sidetableqty);
     System.out.println("Trash Cans: " + trashqty);
+    System.out.println("All Done " + gd);
 
   }
 }
